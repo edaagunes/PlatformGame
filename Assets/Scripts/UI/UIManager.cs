698640db059b6b3568a40bce803034fd91c00fa3
edaagunes <edagunes00@hotmail.com>
2024-01-03 16:56:43 +0300

    Created Weapon Selection Buttons

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,9 +14,18 @@ public class UIManager : MonoBehaviour
    [SerializeField]
    TMP_Text coinText;
 
+   [SerializeField] private Transform butonlarPanel;
+   
     private void Awake()
     {
         Instance = this;
+    }
+
+    private void Start()
+    {
+        TumButonlarinAlphasiniAzalt();
+        butonlarPanel.GetChild(0).GetComponent<CanvasGroup>().alpha = 1f;
+        PlayerMovementController.Instance.TurnNormalPlayer();
     }
 
     public void SliderUpdate(int currentValue, int maxValue)
@@ -28,4 +38,54 @@ public class UIManager : MonoBehaviour
     {
         coinText.text = GameManager.Instance.collectedCoin.ToString();
     }
+
+    void TumButonlarinAlphasiniAzalt()
+    {
+        foreach (Transform btn in butonlarPanel)
+        {
+            btn.gameObject.GetComponent<CanvasGroup>().alpha = 0.25f;
+        }
+    }
+
+    public void NormalButtonPressed()
+    {
+        TumButonlarinAlphasiniAzalt();
+
+        UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.transform.GetComponent<CanvasGroup>()
+            .alpha = 1f;
+        
+        PlayerMovementController.Instance.TurnNormalPlayer();
+    } 
+    public void KilicButtonPressed()
+    {
+        TumButonlarinAlphasiniAzalt();
+
+        UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.transform.GetComponent<CanvasGroup>()
+            .alpha = 1f;
+        
+        PlayerMovementController.Instance.TurnSwordPlayer();
+    }
+    
+    public void OkButtonPressed()
+    {
+        TumButonlarinAlphasiniAzalt();
+
+        UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.transform.GetComponent<CanvasGroup>()
+            .alpha = 1f;
+        
+        PlayerMovementController.Instance.TurnBowPlayer();
+    }
+    
+    public void MizrakButtonPressed()
+    {
+        TumButonlarinAlphasiniAzalt();
+
+        UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.transform.GetComponent<CanvasGroup>()
+            .alpha = 1f;
+        
+        PlayerMovementController.Instance.TurnSpearPlayer();
+    }
+    
+    
+    
 }
